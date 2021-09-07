42525c02747a9dc78944ee4de65aca5e880c1837
paulacarvani <3312@holbertonschool.com>
2021-09-07 13:10:47 -0500

    Fixing the style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,5 @@ if last == 0:
 elif last > 5:
     print("{} {:d} is {:d} and is greater than 5".format(sent, number, last))
 elif last < 6 and last != 0:
-    print("{} {:d} is {:d} and is less than 6 and not 0".format(sent, number, last))
+    print("{} {:d} is {:d} and is less than 6 and not 0"
+    .format(sent, number, last))
