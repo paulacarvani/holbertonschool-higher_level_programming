2ff1caddbe6a0904244bb4f78d5669fc044c7ef0
paulacarvani <3312@holbertonschool.com>
2021-09-07 12:57:18 -0500

    Fixing the style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str = "Last digit of"
+sent = "Last digit of"
 last = number % 10 if number > 0 else int(repr(number)[-1]) * -1
 if (last > 5):
-    print("{} {:d} is {:d} and is greater than 5".format(str, number, last))
+    print("{} {:d} is {:d} and is greater than 5"
+    .format(sent, number, last))
 elif (last == 0):
-    print("{} {:d} is {:d} and is 0".format(str, number, last))
+    print("{} {:d} is {:d} and is 0"
+    .format(sent, number, last))
 else:
-    print("{} {:d} is {:d} and is less than 6 and not 0".format(str, number, last))
+    print("{} {:d} is {:d} and is less than 6 and not 0"
+    .format(sent, number, last))
