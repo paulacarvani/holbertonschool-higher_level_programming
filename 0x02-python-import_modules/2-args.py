65573790eb4e0f85ecd2eef3d36cfe93c4922ad1
paulacarvani <3312@holbertonschool.com>
2021-09-08 12:31:49 -0500

    File to Import sys.arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total_arg = len(sys.argv)
+    print("{} arguments:".format(total_arg - 1))
+    for i in range (1, total_arg):
+        print("{}: {}".format(i, sys.argv[i]))
