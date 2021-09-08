eb030c447fa18158f1a759c903a5ba37a466956a
paulacarvani <3312@holbertonschool.com>
2021-09-08 10:33:32 -0700

    Change style and permissions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     total_arg = len(sys.argv)
     print("{} arguments:".format(total_arg - 1))
-    for i in range (1, total_arg):
+    for i in range(1, total_arg):
         print("{}: {}".format(i, sys.argv[i]))
