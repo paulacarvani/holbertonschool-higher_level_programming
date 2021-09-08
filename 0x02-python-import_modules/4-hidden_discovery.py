bff6aa5a2d16740db21b7918938e199c41e2209e
paulacarvani <3312@holbertonschool.com>
2021-09-08 17:33:39 -0500

    Test the functions

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import hidden_4
-all_dir = dir(hidden_4)
-avoid = '__'
-for i in range(0, len(all_dir)):
-    if avoid not in all_dir[i]:
-        print(all_dir[i])
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] != '_' and i[1] != '_':
+            print(i)
