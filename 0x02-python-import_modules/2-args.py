b0fd76cae7cade00ac4b05e5992f04a3998b1e7e
paulacarvani <3312@holbertonschool.com>
2021-09-08 12:44:06 -0500

    Solution to 0 and 1 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == "__main__":
     import sys
     total_arg = len(sys.argv)
-    print("{} arguments:".format(total_arg - 1))
+    if total_arg == 1:
+        print("0 arguments.")
+    elif total_arg == 2:
+        print("{} argument:".format(total_arg - 1))
+    else:
+        print("{} arguments:".format(total_arg - 1))
     for i in range(1, total_arg):
         print("{}: {}".format(i, sys.argv[i]))
