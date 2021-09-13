a2829e4de80afe40399bd2f4acb497c0db833d37
paulacarvani <3312@holbertonschool.com>
2021-09-13 14:42:46 -0500

    Change the conditions

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return(my_list[idx])
+    for i in my_list:
+        if idx < 0:
+            return("None")
+        elif i >= 0 and i <= idx:
+            return(my_list[idx])
+        else:
+            return("None")
