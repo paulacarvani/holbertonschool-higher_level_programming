456f5e5d6c1b4523679dd12ed8fa4f2d88c748fc
paulacarvani <3312@holbertonschool.com>
2021-09-27 21:07:37 -0500

    Change range

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    for i in range(x):
+    for index in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[index]), end="")
             i += 1
         except(ValueError, TypeError):
             pass
