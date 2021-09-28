ca0a3afd185e1fece87a779b2d95d8c199b7ddfc
paulacarvani <3312@holbertonschool.com>
2021-09-28 18:44:32 -0500

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module creates:
+    - a class named square with a private atribute size
+    - a public parameter area to calculates area with size
+    - uses a getter for size and setter to size with
+    some conditions for type error and value error
+    - Public parameters for print a square with #"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Construct wit private parameter size"""
+        self.__size = size
+
+    def area(self):
+        """New construct with public parameters for calculates the area
+        size elevates at 2"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size with conditions"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Public parameter for print a saquare"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                for i in range(self.size):
+                    print("#", end="")
+                print("")
