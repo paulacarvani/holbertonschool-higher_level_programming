d9cca58ff1386ab523da7537f63a701e58bf134b
paulacarvani <3312@holbertonschool.com>
2021-10-06 15:53:13 -0500

    Update files task 1

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,8 +2,8 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, "b", 3],
-    [4, 5, 6]
+    [1.1, -2.3, 3.2],
+    [-4.1, 5.6, 6.6]
 ]
-print(matrix_divided(matrix, 3))
+print(matrix_divided(matrix, -3))
 print(matrix)
