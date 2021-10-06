6b59caf650fc1d6ecefe10a03ff58bbd784f23e1
paulacarvani <3312@holbertonschool.com>
2021-10-06 13:00:45 -0500

    Updating comments in module and functions

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """Module to add two args
     arg a + arg b
+    if a or b are float change to int
+    ir a or b are not int or floar raise TypeError
     Return a + b"""
 
 
 def add_integer(a, b=98):
-    """Function to add two integers"""
+    """Function to add two integers
+    arg a and arg b
+    return a + b"""
     if type(a) is float:
         a = int(a)
     if type(b) is float:
