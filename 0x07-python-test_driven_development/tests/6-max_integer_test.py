3522aa13cc73602dc92c3548a41bf2808d2086b0
paulacarvani <3312@holbertonschool.com>
2021-10-07 00:21:39 -0500

    Update error cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,9 +6,10 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """Test Cases"""
+
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3]), 3)
+        self.assertIsNone(max_integer([]),)
         self.assertEqual(max_integer([-6584789410681, 1]), 1)
         self.assertEquals(max_integer([]), None)
         self.assertEquals(max_integer([0, 0, 0]), 0)
