a8a18c16af1c952936c8a69e91177e0997da62a3
paulacarvani <3312@holbertonschool.com>
2021-10-06 21:02:48 -0500

    Task 7 Advanced

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that creates a function
+that multiples two matrix with
+module numpy"""
+
+
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function that returns the mul of
+    two matrices using the module numpy"""
+
+    mul = np.matmul(m_a, m_b)
+    return mul
