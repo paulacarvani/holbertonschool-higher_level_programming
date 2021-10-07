7e2b690124750540d4127442fe5372a91da03290
paulacarvani <3312@holbertonschool.com>
2021-10-07 01:02:22 -0500

    REview pep8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,13 +13,13 @@ def matrix_divided(matrix, div):
     Empty sub-lists are not allowed.
     Divisor must be greater than 0 and must be an int or float.
     """
+    a = ("matrix must be a matrix (list of lists) of integers/floats")
 
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(element, int) or isinstance(element, float))
                     for element in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) \
-            of integers/floats")
+        raise TypeError(a)
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
