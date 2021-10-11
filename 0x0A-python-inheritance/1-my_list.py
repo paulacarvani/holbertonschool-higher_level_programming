e53c131fa2acd91a8b4917ac00d3b2c0423217fc
paulacarvani <3312@holbertonschool.com>
2021-10-11 17:59:45 -0500

    Task 0, 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that creates a Class that inherits from list of another file
+and sort the list created"""
+
+
+class MyList(list):
+    """Class that inherits from class list"""
+
+
+    def print_sorted(self):
+        print(sorted(self))
