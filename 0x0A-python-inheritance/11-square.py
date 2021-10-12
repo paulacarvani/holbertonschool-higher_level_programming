d1858698df4d85145e10f207a95835f849e2a6f5
paulacarvani <3312@holbertonschool.com>
2021-10-11 21:35:31 -0500

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to create a class and import another class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square with atributes from rectangle"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Print a string"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
