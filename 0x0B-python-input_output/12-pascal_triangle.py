ed3d71440c6990f4637997c4fe83ff0ce79f163f
paulacarvani <3312@holbertonschool.com>
2021-10-13 20:58:43 -0500

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Contains the pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal’s triangle"""
+    if n <= 0:
+	    return []
+
+    limit = n - 1
+    triangle = [
+        [1]
+        ]
+
+    for i in range(limit):
+        row = [1]
+
+        if len(triangle[i]) > 1:
+            len_row = len(triangle[i]) - 1
+
+            for j in range(len_row):
+                row.append(triangle[i][j] + triangle[i][j + 1])
+
+        row.append(1)
+        triangle.append(row)
+    return triangle
