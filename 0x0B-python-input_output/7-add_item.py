d84593f604c3867d7c3fd224529993a0ff9c8cae
paulacarvani <3312@holbertonschool.com>
2021-10-13 20:42:13 -0500

    Task 7, review

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@
 import sys
 
 
-save_to_json = __import__("7-save_to_json_file").save_to_json_file
-load_from_json = __import__("8-load_from_json_file").load_from_json_file
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
 
 arguments = sys.argv[1:]
