d7c0848720cc680e20407a4874c9d764280be00f
paulacarvani <3312@holbertonschool.com>
2021-10-18 11:55:13 -0500

    First version of the files base, rectangle and square

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"Module with class Rectangle"
+from models.base import Base
+
+class Rectangle(Base):
+    """Class Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y"""
+        return self.__y
+
+    @x.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
+
+    def area(self):
+        """returns the area value of the Rectangle"""
+        return self.width * self.height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            print("")
+
+        for k in range(self.__y):
+            print("")
+        for i in range(self.__height):
+            for l in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width - 1):
+                print('#', end="")
+            print('#')
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width,\
+                self.__height)
