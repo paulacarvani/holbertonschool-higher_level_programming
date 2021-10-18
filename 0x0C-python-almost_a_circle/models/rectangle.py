815a37aaccc11c3b5d6ba3defc268ca662cb52a6
paulacarvani <3312@holbertonschool.com>
2021-10-18 12:01:45 -0500

    REview PEP8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,4 +85,4 @@ class Rectangle(Base):
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.__x, self.__y, self.__width,
-                self.__height)
+            self.__height)
