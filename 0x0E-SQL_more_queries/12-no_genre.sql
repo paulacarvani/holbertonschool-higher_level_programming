8870511d84ff05397938920a104dd9e9ca6c0847
paulacarvani <3312@holbertonschool.com>
2021-11-17 23:21:09 -0500

    Task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,12 @@
+--  script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT
+    tv_shows.title,
+    genre_id
+FROM
+    tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT OUTER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id is NULL
+ORDER BY
+    tv_shows.title,
+    tv_show_genres.genre_id;
