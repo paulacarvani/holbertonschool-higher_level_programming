bc06563bc1f4403c237051931bb1b71764b0f739
paulacarvani <3312@holbertonschool.com>
2021-11-17 23:30:11 -0500

    Task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,14 @@
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT
+    tv_genres.name AS genre,
+    COUNT(*) AS number_of_shows
+FROM
+    tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+GROUP BY
+    genre
+ORDER BY
+    number_of_shows
+DESC
+    ;
