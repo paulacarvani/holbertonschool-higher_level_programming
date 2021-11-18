d3a9db7215094a6fc75007363bd9f802378e2e42
paulacarvani <3312@holbertonschool.com>
2021-11-17 21:49:15 -0500

    Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- script that creates the database hbtn_0d_usa and the table
+-- states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE TABLE IF NOT EXIST hbtn_0d_usa.states
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
