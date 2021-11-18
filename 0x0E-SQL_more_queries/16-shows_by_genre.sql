0980be18947f53d189dc7011d7de0b0a77a48432
paulacarvani <3312@holbertonschool.com>
2021-11-17 23:39:59 -0500

    Update code

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- List all shows and genres linked to show from 'hbtn_0d_tvshows'
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+SELECT t.`title`, g.`name`
+FROM `tv_shows` AS t
+LEFT JOIN `tv_show_genres` AS s
+ON t.`id` = s.`show_id`
+LEFT JOIN `tv_genres` AS g
+ON s.`genre_id` = g.`id`
+ORDER BY t.`title`, g.`name`;
