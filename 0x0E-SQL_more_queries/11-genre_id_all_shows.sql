a39c8442566cd17ffb4de8cb3832f6b0ef9779fd
paulacarvani <3312@holbertonschool.com>
2021-11-17 23:17:09 -0500

    Task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,11 @@
+--  script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT
+    tv_shows.title,
+    genre_id
+FROM
+    tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title,
+    tv_show_genres.genre_id;
