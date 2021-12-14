437ac8e0c875c457ca3073bd221564dbb8919e2d
paulacarvani <root@LAPTOP-HCFM27PN.localdomain>
2021-12-13 19:51:33 -0500

    Prints a square of X

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that prints a square
+const num = parseInt(process.argv[2]);
+const str = 'X';
+if (Number.isInteger(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log(str.repeat(num));
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
