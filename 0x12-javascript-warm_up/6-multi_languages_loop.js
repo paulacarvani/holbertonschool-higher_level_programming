0e9437e3bf318a847c652c85556bdb388b036c2d
paulacarvani <root@LAPTOP-HCFM27PN.localdomain>
2021-12-13 19:01:28 -0500

    Script to prints a array with for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 // script that prints 3 lines with for
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const lang of langs) {
-  console.log(lang);
-}
+langs.forEach(element => console.log(element));
