26840fb3d04de3bcfa842e3985e987f94e645cb6
paulacarvani <3312@holbertonschool.com>
2021-12-14 22:11:55 -0500

    Test script

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,7 +6,7 @@ const dict = require('./101-data').dict;
 const newDict = {};
 
 for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
+ if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
     newDict[dict[key]].push(key);
