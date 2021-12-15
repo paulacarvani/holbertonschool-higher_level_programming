bdb2bd7fada80d2978d5871551a00c0e1859f530
paulacarvani <3312@holbertonschool.com>
2021-12-14 22:12:39 -0500

    Testing the script

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // imports an array and computes a new array
-const list = require('./100-data.js').list;
+ const list = require('./100-data.js').list;
 const temp = list.map((x, index) => x * index);
 console.log(list);
 console.log(temp);
