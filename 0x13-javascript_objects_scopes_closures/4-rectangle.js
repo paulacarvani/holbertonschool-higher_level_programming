80c621c5aebc17a75975bcb0a7f3c1244e776454
paulacarvani <3312@holbertonschool.com>
2021-12-14 19:38:22 -0500

    New script for rectangle adding two new instance method for rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+// Class that defines a Rectangle
+// take two args and validates there are no equal to 0
+// Instance Method called print, to print the Rect with X
+// Instance Method rotate, exchange values of width and height
+// Instance Method double multiplies w and h by 2
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
