52bc53a97c94d0c0b5f01bdbcd0a1f24e93874a3
paulacarvani <3312@holbertonschool.com>
2022-01-02 13:52:10 -0800

    Testing files to print states in db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+0-select_states.py
+"""
+import MySQLdb
+import sys
+
+
+def init_db():
+    """initializes a db with MySQLdb"""
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    return db
+
+
+def print_states(db):
+    """prints all states from input DB"""
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_states(init_db())
