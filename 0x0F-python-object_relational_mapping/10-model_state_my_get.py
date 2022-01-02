f5621e5fa84eb24937c9c15a956fbf96e984e4f5
paulacarvani <3312@holbertonschool.com>
2022-01-02 15:09:11 -0800

    script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+7-model_state_fetch_all.py
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session and engine for sqlalchemy DB instance"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return (engine, session)
+
+
+def parse_input(s):
+    """parses input removing single quotes and semicolons"""
+    REP_CHARS = [';', "'", '"']
+    for i in REP_CHARS:
+        s = s.replace(i, '--')
+    return s
+
+
+def print_state_name(db):
+    """prints the ID of input state"""
+    session = db[1]
+    n = parse_input(sys.argv[4])
+    r = session.query(State).filter(State.name == n)
+    try:
+        print(r[0].id)
+    except:
+        print("Not found")
+    session.close()
+    db[0].dispose()
+
+
+if __name__ == '__main__':
+    print_state_name(init_sess())
