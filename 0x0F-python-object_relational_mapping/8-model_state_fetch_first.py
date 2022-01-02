02d224158db2c2eb53dbd1251f8c99f33bc6b276
paulacarvani <3312@holbertonschool.com>
2022-01-02 14:56:43 -0800

    script that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+7-model_state_fetch_all.py
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session and engine for sqlalchemy DB instance"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return (engine, session)
+
+
+def print_min_state(db):
+    """prints the state with min value using first()"""
+    session = db[1]
+    instance = session.query(State).first()
+    try:
+        print(instance.id, ': ', instance.name, sep='')
+    except:
+        print("Nothing")
+    session.close()
+    db[0].dispose()
+
+
+if __name__ == '__main__':
+    print_min_state(init_sess())
