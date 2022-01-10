4382cdb1bb14328ea26488d9bd3fdb7ae717efc1
paulacarvani <3312@holbertonschool.com>
2022-01-09 19:58:34 -0500

    Task 6, script based on task 2

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {'email': argv[2]}
+    data = requests.post(url, email)
+    print(data.text)
