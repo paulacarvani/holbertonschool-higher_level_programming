67f062be1b4edd3dea86b7a588ca2c1fdd4716e2
paulacarvani <3312@holbertonschool.com>
2022-01-09 22:39:32 -0500

    Task 0, test new script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
-from urllib import request
-
+from urllib.request import urlopen, Request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as f:
-        response = f.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('utf-8')))
+    url = 'https://intranet.hbtn.io/status'
+    req = Request(url)
+    with urlopen(url) as response:
+        read = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(read)))
+        print('\t- content: {}'.format(read))
+        print('\t- utf8 content: {}'.format(read.decode("UTF-8")))
